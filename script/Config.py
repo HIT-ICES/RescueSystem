28d6e9c161f2de54806412da90e89cce995bfef5
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2024-05-10 16:36:39 +0800

    gateway

--- script/Config.py
+++ script/Config.py
@@ -1,6 +1,17 @@
-DockerRepo='192.168.1.199:5000/rescue-system-2024'
-Namespace='rescue-system-redive'
+import os
 
-SourceDir='./src'
-GatewayTemplate='./gateway/nginx.conf.template'
-Gateway='./gateway/nginx.conf'
+DockerRepo = '192.168.1.199:5000/rescue-system-2024'
+Namespace = 'rescue-system-redive'
+ProjectRoot = '..'
+
+
+__current_file_path = os.path.abspath(__file__)
+__current_dir_path = os.path.dirname(__current_file_path)
+__project_root = os.path.dirname(__current_dir_path, ProjectRoot)
+print("Current file path:", current_file_path)
+print("Current directory path:", current_dir_path)
+
+
+SourceDir = os.path.join(__project_root, 'src')
+GatewayTemplate = os.path.join(__project_root, 'gateway/nginx.conf.template')
+Gateway = os.path.join(__project_root, '/gateway/nginx.conf')
