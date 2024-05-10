90507d8edcd9b99e1187a64b238d7e2ba4e675e7
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2024-05-10 16:38:30 +0800

    gateway

--- script/Config.py
+++ script/Config.py
@@ -12,4 +12,4 @@ __project_root = os.path.join(__current_dir_path, ProjectRoot)
 
 SourceDir = os.path.join(__project_root, 'src')
 GatewayTemplate = os.path.join(__project_root, 'gateway/nginx.conf.template')
-Gateway = os.path.join(__project_root, '/gateway/nginx.conf')
+Gateway = os.path.join(__project_root, 'gateway/nginx.conf')
